fcb2bd10a52a51065222ad99d05445574c6053e7
Kilian Tarbett <kiliantarbett@gmail.com>
2019-12-10 22:29:01 +0800

    Intial Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from src import Inspector
+
+def Start():
+  app = Inspector.Inspector();
+
+if __name__ == "__main__":
+  Start();
---
+++ src/Inspector.py
@@ -0,0 +1,10 @@
+import tkinter
+
+# Main application window
+class Inspector():
+
+
+  def __init__(self):
+    self.window = tkinter.Tk();
+    self.window.title("Object Inspector");
+    self.window.mainloop();
