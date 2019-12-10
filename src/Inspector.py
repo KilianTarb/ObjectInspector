c6924e8f156289db899b6b5d9bc63ab113f71b0f
Kilian Tarbett <kiliantarbett@gmail.com>
2019-12-10 23:17:19 +0800

    Added buttons and frames

--- src/Inspector.py
+++ src/Inspector.py
@@ -1,10 +1,33 @@
 import tkinter
 
 # Main application window
+
+
 class Inspector():
+	buttonWidth = 15
+	buttonHeight = 1
 
+    # Initalises the window and it's components
+	def CreateUIComponents(self):
+		self.window = tkinter.Tk()
+		self.window.title("Object Inspector")
 
-  def __init__(self):
-    self.window = tkinter.Tk();
-    self.window.title("Object Inspector");
-    self.window.mainloop();
+		# Create frames
+		self.textAreaFrame = tkinter.Frame(self.window)
+		self.textAreaFrame.pack(side="top")
+		self.buttonAreaFrame = tkinter.Frame(self.window)
+		self.buttonAreaFrame.pack(side="bottom")
+
+		# Create buttons
+		self.submitBtn = tkinter.Button(self.window, text="Submit")
+		self.submitBtn.config(width=self.buttonWidth, height=self.buttonHeight)
+		self.submitBtn.pack(side="right")
+		self.clearBtn = tkinter.Button(self.window, text="Clear")
+		self.clearBtn.config(width=self.buttonWidth, height=self.buttonHeight)
+		self.clearBtn.pack(side="right")
+
+		# Start loop
+		self.window.mainloop()
+
+	def __init__(self):
+		self.CreateUIComponents()
