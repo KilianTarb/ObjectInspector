e400906f3bd6b32b7b71695aa5685f2ad6e3bd7f
KilianTarb <kiliantarbett@gmail.com>
2021-02-28 12:55:27 +0800

    Style comments correctly

--- src/inspector.py
+++ src/inspector.py
@@ -1,7 +1,11 @@
 import tkinter
 
-# Main application root
+
 class Inspector():
+	"""
+	Object Inspector root window.
+	"""
+
 	root_padding_x = 10
 	root_padding_y = 10
 
@@ -14,8 +18,11 @@ class Inspector():
 	txt_padding_x = 10
 	txt_padding_y = 10
 
-    # Initalises the root and it's components
 	def create_ui_components(self):
+		""" 
+		Initalises the root and it's components.
+		"""
+
 		self.root = tkinter.Tk()
 		self.root.geometry("850x500")
 		self.root.config(padx=self.root_padding_x, pady=self.root_padding_y)
@@ -27,23 +34,31 @@ class Inspector():
 		self.create_text_areas()
 		self.create_buttons()
 
-
-	# Creates the widget containers
 	def create_frames(self):
+		"""
+		Creates the widget containers.
+		"""
+
 		self.txt_area_frame = tkinter.Frame(self.root, bg="green")
 		self.txt_area_frame.pack(side="top", expand=True, fill=tkinter.BOTH)
 
 		self.btn_area_frame = tkinter.Frame(self.root, bg="blue", height=self.btn_frame_height)
 		self.btn_area_frame.pack(side="bottom", fill=tkinter.BOTH)
 
-	# Define and initalise the text areas
 	def create_text_areas(self):
+		"""
+		Define and initalise the text areas.
+		"""
+
 		self.txt_input_area = tkinter.Text(self.txt_area_frame)
 		self.txt_input_area.config(padx=self.txt_padding_x, pady=self.txt_padding_y)
 		self.txt_input_area.pack(expand=True, fill=tkinter.BOTH)
 
-	# Define and initalise the buttons
 	def create_buttons(self):
+		"""
+		Define and initalise the buttons.
+		"""
+		
 		self.submit_btn = tkinter.Button(self.btn_area_frame, text="Submit")
 		self.submit_btn.config(width=self.btn_width, height=self.btn_height)
 		self.submit_btn.pack(side="right", padx=self.btn_padding_x, pady=self.btn_padding_y)
