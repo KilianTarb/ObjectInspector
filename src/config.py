f0597fc0654335d3b5f1b82730597c5f2d8ef8a1
KilianTarb <kiliantarbett@gmail.com>
2021-02-28 13:09:35 +0800

    Added constants

---
+++ src/config.py
@@ -0,0 +1,17 @@
+class inspector_config():
+	"""
+	Configuration for the main inspector window.
+	"""
+	
+	# Windows padding on X axis
+	ROOT_PADDING_X = 10
+	ROOT_PADDING_Y = 10
+
+	BTN_FRAME_HEIGHT = 300
+	BTN_WIDTH = 15
+	BTN_HEIGHT = 1
+	BTN_PADDING_X = 5
+	BTN_PADDING_Y = 5
+
+	TXT_PADDING_X = 10
+	TXT_PADDING_Y = 10
--- src/inspector.py
+++ src/inspector.py
@@ -1,22 +1,11 @@
 import tkinter
+from .config import inspector_config as i_config
 
 
 class inspector():
 	"""
 	Object Inspector root window.
 	"""
-
-	root_padding_x = 10
-	root_padding_y = 10
-
-	btn_frame_height = 300
-	btn_width = 15
-	btn_height = 1
-	btn_padding_x = 5
-	btn_padding_y = 5
-
-	txt_padding_x = 10
-	txt_padding_y = 10
 
 	def create_ui_components(self):
 		""" 
@@ -25,10 +14,8 @@ class inspector():
 
 		self.root = tkinter.Tk()
 		self.root.geometry("850x500")
-		self.root.config(padx=self.root_padding_x, pady=self.root_padding_y)
+		self.root.config(padx=i_config.ROOT_PADDING_X, pady=i_config.ROOT_PADDING_Y)
 		self.root.title("Object Inspector")
-	
-		
 
 		self.create_frames()
 		self.create_text_areas()
@@ -42,7 +29,7 @@ class inspector():
 		self.txt_area_frame = tkinter.Frame(self.root, bg="green")
 		self.txt_area_frame.pack(side="top", expand=True, fill=tkinter.BOTH)
 
-		self.btn_area_frame = tkinter.Frame(self.root, bg="blue", height=self.btn_frame_height)
+		self.btn_area_frame = tkinter.Frame(self.root, bg="blue", height=i_config.BTN_FRAME_HEIGHT)
 		self.btn_area_frame.pack(side="bottom", fill=tkinter.BOTH)
 
 	def create_text_areas(self):
@@ -51,21 +38,21 @@ class inspector():
 		"""
 
 		self.txt_input_area = tkinter.Text(self.txt_area_frame)
-		self.txt_input_area.config(padx=self.txt_padding_x, pady=self.txt_padding_y)
+		self.txt_input_area.config(padx=i_config.TXT_PADDING_X, pady=i_config.TXT_PADDING_Y)
 		self.txt_input_area.pack(expand=True, fill=tkinter.BOTH)
 
 	def create_buttons(self):
 		"""
 		Define and initalise the buttons.
 		"""
-		
+
 		self.submit_btn = tkinter.Button(self.btn_area_frame, text="Submit")
-		self.submit_btn.config(width=self.btn_width, height=self.btn_height)
-		self.submit_btn.pack(side="right", padx=self.btn_padding_x, pady=self.btn_padding_y)
+		self.submit_btn.config(width=i_config.BTN_WIDTH, height=i_config.BTN_HEIGHT)
+		self.submit_btn.pack(side="right", padx=i_config.BTN_PADDING_X, pady=i_config.BTN_PADDING_Y)
 
 		self.clear_btn = tkinter.Button(self.btn_area_frame, text="Clear")
-		self.clear_btn.config(width=self.btn_width, height=self.btn_height)
-		self.clear_btn.pack(side="right", padx=self.btn_padding_x, pady=self.btn_padding_y)
+		self.clear_btn.config(width=i_config.BTN_WIDTH, height=i_config.BTN_HEIGHT)
+		self.clear_btn.pack(side="right", padx=i_config.BTN_PADDING_X, pady=i_config.BTN_PADDING_Y)
 
 	def __init__(self):
 		self.create_ui_components()
