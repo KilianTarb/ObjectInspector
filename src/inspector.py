ff6f3fa5d56f2b28ee0fbe76cd3c2ffc9ccf888c
KilianTarb <kiliantarbett@gmail.com>
2021-03-03 14:23:49 +0800

    Added tabs

--- src/inspector.py
+++ src/inspector.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import ttk
 from .config import inspector_config as i_config
 
 
@@ -17,20 +18,38 @@ class inspector():
 		self.root.config(padx=i_config.ROOT_PADDING_X, pady=i_config.ROOT_PADDING_Y)
 		self.root.title("Object Inspector")
 
+		self.tab_control = tkinter.ttk.Notebook(self.root)
+
 		self.create_frames()
 		self.create_text_areas()
 		self.create_buttons()
+
+		self.tab_control.pack(expand=True, fill=tkinter.BOTH)
 
 	def create_frames(self):
 		"""
 		Creates the widget containers.
 		"""
 
-		self.txt_area_frame = tkinter.Frame(self.root, bg="green")
+		self.base_frame = tkinter.Frame(self.tab_control)
+		self.base_frame.pack(side="top", expand=True, fill=tkinter.BOTH)
+
+		self.txt_area_frame = tkinter.Frame(self.base_frame, bg="green")
 		self.txt_area_frame.pack(side="top", expand=True, fill=tkinter.BOTH)
 
-		self.btn_area_frame = tkinter.Frame(self.root, bg="blue", height=i_config.BTN_FRAME_HEIGHT)
+		self.btn_area_frame = tkinter.Frame(self.base_frame, bg="blue", height=i_config.BTN_FRAME_HEIGHT)
 		self.btn_area_frame.pack(side="bottom", fill=tkinter.BOTH)
+
+		self.tab_control.add(self.base_frame, text="Inspector")
+
+	def add_data_tab(self):
+		self.new_tab = tkinter.Frame(self.tab_control)
+		self.new_tab.pack(fill=tkinter.BOTH, expand=True)
+
+		self.new_tab_text = tkinter.Text(self.new_tab)
+		self.new_tab_text.pack(expand=True, fill=tkinter.BOTH)
+
+		self.tab_control.add(self.new_tab)
 
 	def create_text_areas(self):
 		"""
@@ -41,17 +60,20 @@ class inspector():
 		self.txt_input_area.config(padx=i_config.TXT_PADDING_X, pady=i_config.TXT_PADDING_Y)
 		self.txt_input_area.pack(expand=True, fill=tkinter.BOTH)
 
+	def clear_txt_area(self):
+		self.txt_input_area.delete("1.0", tkinter.END)
+
 	def create_buttons(self):
 		"""
 		Define and initalise the buttons.
 		"""
 
 		self.submit_btn = tkinter.Button(self.btn_area_frame, text="Submit")
-		self.submit_btn.config(width=i_config.BTN_WIDTH, height=i_config.BTN_HEIGHT)
+		self.submit_btn.config(width=i_config.BTN_WIDTH, height=i_config.BTN_HEIGHT, command=self.add_data_tab)
 		self.submit_btn.pack(side="right", padx=i_config.BTN_PADDING_X, pady=i_config.BTN_PADDING_Y)
 
 		self.clear_btn = tkinter.Button(self.btn_area_frame, text="Clear")
-		self.clear_btn.config(width=i_config.BTN_WIDTH, height=i_config.BTN_HEIGHT)
+		self.clear_btn.config(width=i_config.BTN_WIDTH, height=i_config.BTN_HEIGHT, command=self.clear_txt_area)
 		self.clear_btn.pack(side="right", padx=i_config.BTN_PADDING_X, pady=i_config.BTN_PADDING_Y)
 
 	def __init__(self):
